0fb1f968b6378e930d5ea9976ee02fe16a13437b
Kunal-Jain7 <kunal70223@gmail.com>
2026-03-31 19:59:09 +0530

    Updated node config for the node pools and Cluster node config

--- terraform-gke/gke-cluster.tf
+++ terraform-gke/gke-cluster.tf
@@ -8,4 +8,13 @@ resource "google_container_cluster" "client-gke-cluster" {
   subnetwork = google_compute_subnetwork.client-subnet.self_link
 
   deletion_protection = false
+
+  node_config {
+    machine_type = "e2-medium"
+
+    disk_size_gb = 20
+    disk_type    = "pd-standard"
+
+    preemptible = true
+  }
 }
